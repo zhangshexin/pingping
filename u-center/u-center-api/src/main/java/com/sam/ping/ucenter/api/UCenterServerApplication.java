01c5d3b3f9e880ea09711adbb0d4b8fc156c41c9
zhangshexin <zhangshexin@anne.com.cn>
2018-05-17 17:34:34 +0800

    结构调整

---
+++ u-center/u-center-api/src/main/java/com/sam/ping/ucenter/api/UCenterServerApplication.java
@@ -0,0 +1,12 @@
+package com.sam.ping.ucenter.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UCenterServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UCenterServerApplication.class, args);
+	}
+}
--- u-center/u-center-server/src/main/java/com/sam/ping/ucenter/UCenterApplication.java
+++ u-center/u-center-server/src/main/java/com/sam/ping/ucenter/UCenterApplication.java
@@ -3,7 +3,6 @@ package com.sam.ping.ucenter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 
 @EnableDiscoveryClient
